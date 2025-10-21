60536bc24356b56c210c374cce9e579ca613dc33
edsilva530 <edilsonsilva530@gmail.com>
2025-10-21 20:07:45 -0300

    feat: input label, page list componet

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-  <p>teste</p>
-  <router-outlet />`
+  template: `<router-outlet />`
 })
 export class App {
   
